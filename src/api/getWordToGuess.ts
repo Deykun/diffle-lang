7eb1dd48f9b5ccd49a13f3da7d5c096b745e49ef
Szymon Tondowski <stdeykun+git@gmail.com>
2024-02-19 18:03:20 +0100

    Random word fix

--- src/api/getWordToGuess.ts
+++ src/api/getWordToGuess.ts
@@ -21,8 +21,6 @@ const getRandomIntForDaily = () => {
 };
 
 export const getWordToGuess = async ({ gameMode, gameLanguage, seedNumber }: { gameMode: GameMode, gameLanguage: string, seedNumber?: number }): Promise<string> => {
-    return 'schmetterling';
-
     const catalogResponse = await fetch(`./dictionary/${gameLanguage}/catalog.json`);
 
     const cataolgResult: { words: number, items: catalogItem[] } = await catalogResponse.json();
