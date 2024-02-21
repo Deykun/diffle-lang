238a62c8394072bf2423aff7779a694a2ed29424
Szymon Tondowski <stdeykun+git@gmail.com>
2024-02-21 22:43:49 +0100

    es - added to src build script

--- scripts/post-build.mjs
+++ scripts/post-build.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const SUPPORTED_LANGS = ['cs', 'de', 'en', 'pl'];
+const SUPPORTED_LANGS = ['cs', 'de', 'en', 'es', 'pl'];
 
 SUPPORTED_LANGS.forEach((lang) => {
     let html = fs.readFileSync(`./dist/index.html`, 'utf-8');
